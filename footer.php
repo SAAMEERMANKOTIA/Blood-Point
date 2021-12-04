1f18f1642d0afe611219fb87d59c4b659031cb59
Saameer Mankotia <87329381+SAAMEERMANKOTIA@users.noreply.github.com>
2021-12-04 20:10:29 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: black;
+   text-align: center;
+   padding: 5px 0px 5px 0px;
+}
+</style>
+</head>
+<body>
+
+<div class="footer">
+  <p> Created and Maintained By Sameer Mankotia {Email : sameermankotia2000@gmail.com}
+</footer></p>
+</div>
+
+</body>
+
